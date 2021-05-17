a9bbe8a1bad756b4bbf80cfa3f5be6fcef515860
Vinícius Blauth <vr.blauth@gmail.com>
2021-05-17 16:58:10 -0300

    Add files via upload

---
+++ new 2.1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+//var
+float n1, r1, r2
+
+//cod
+	int main(){
+		prinf("Escreva um número:")
+		scanf("%f", &n1);
+		r1=(n1+1)
+		r2=(n1-1)
+		printf("O número do número que você digigtou é %.1f/n" n1);
+		printf("Seu sucessor é %.1f/n" r1);
+		printf("Enquanto seu antecessor é %¨.1f/n" r2);
+
+		getch();
+		return 0;
+	}
+//fimcod
