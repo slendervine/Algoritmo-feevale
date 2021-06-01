0c9c119212bd9b6ee96b166ccf15f60453465daf
Vinícius Blauth <vr.blauth@gmail.com>
2021-06-01 09:52:35 -0300

    Add files via upload

---
+++ Ex 001.c
@@ -0,0 +1,21 @@
+#include stdlib
+#include stdio
+
+int main(void){
+    int n, x, y
+    int num = 0
+
+    do{
+    printf("Digite um numero");
+    scanf("%d", &n);
+    } while(n<=0);
+
+    for(x=1, x<=n, x++){
+
+        for(y=1, y=<0, y++){
+            num++
+            printf("%d", num);
+            };
+        printf("\n")
+    }
+}
