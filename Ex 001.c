b7490afe2d0eeb371143386fbd15add2dffa57b1
Vinícius Blauth <vr.blauth@gmail.com>
2021-06-01 09:55:14 -0300

    Update Ex 001.c

--- Ex 001.c
+++ Ex 001.c
@@ -1,12 +1,21 @@
-#include stdlib
-#include stdio
+/* Proponha um programa em C que leia um número inteiro positivo ne em seguida exiba n linhas do chamado Triângulo de Floyd, conforme exemplificado abaixo:
+
+n = 3
+1
+23
+456 */
+
+
+
+#include <stdlib.h>
+#include <stdio.h>
 
 int main(void){
-    int n, x, y
-    int num = 0
+    int n, x, y;
+    int num = 0;
 
     do{
-    printf("Digite um numero");
+    printf("Digite um numero");               //Número de linhas;
     scanf("%d", &n);
     } while(n<=0);
 
@@ -14,7 +23,7 @@ int main(void){
 
         for(y=1, y=<0, y++){
             num++
-            printf("%d", num);
+            printf("%d", num)
             };
         printf("\n")
     }
